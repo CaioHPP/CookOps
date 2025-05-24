45840df09c241bbe0c505ba456315db1d77b4876
CaioHPP <caioutf@gmail.com>
2025-05-23 21:56:26 -0300

    feat: adiciona suporte ao Swagger para documentação da API

--- cookops-backend/src/main.ts
+++ cookops-backend/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,16 @@ async function bootstrap() {
       transform: true, // transforma automaticamente tipos (ex: string -> number)
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('CookOps API')
+    .setDescription('Documentação da API da dark kitchen')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
